71e1a22104ee4ebb3318389568fade08eb0286d0
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-06-20 16:04:49 +0300

    Initial version

---
+++ js/activities.js
@@ -0,0 +1,382 @@
+
+			
+			talk_markers=[				
+				{
+					lat: 48.3661888,
+					lng: 14.513888,
+					title: 'Research Institute for Symbolic Computation',
+					"info": [{"title":"GBRELA: Groebner Bases, Resultants and Linear Algebra" ,"type":"workshop_organize",
+						"where":"Research Institute for Symbolic Computation (RISC)",
+						"what":" the GBRELA workshop",
+						"from":"3",	"to":"6 of September 2013"}],
+					"year": "2013",
+					"month":"9",
+					"day":"4"					
+				},
+				{
+					lat: 40.8074327,
+					lng: -77.8610001,
+					title: 'PennState University',
+					"info": [{"title":"Research Visit" ,"type":"visit","whom":"Prof Andrews",
+						"where":"PennState University",	"what":"partition analysis", "from":"13",
+						"to":"19 of August 2013"},
+						{"title":"Seminar Talk" ,"type":"talk","where":"Partitions Seminar",	
+						"topic":"A Geometric Approach to Partition Analysis"
+						}],
+					"year": "2013",
+					"month":"8",
+					"day":"15"
+				},
+				{
+					lat: 37.7229285,
+					lng: -122.4807225,
+					title: 'San Francisco State University',
+					"info": [{"title":"" ,"type":"visit",
+						"where":"SFSU",	"whom":"Prof. Matthias Beck", "when":""
+						, "during":" the first semester of 2012"
+					}],
+					"year": "2012",
+					"month":"4",
+					"day":"1"
+				},
+				{
+					lat: 47.6553322,
+					lng: -122.3047731,
+					title: 'University of Washington',
+					"info": [{"title":"SAGEDays" ,"type":"workshop",
+						"where":"University of Washington",	"what":"SAGEDays workshop", "when":"2011"
+					}],
+					"year": "2011",
+					"month":"9",
+					"day":"1"
+				},
+				{
+					lat: 41.313097,
+					lng: -105.579671,
+					title: 'University of Wyoming',
+					"info": [{"title":"RMMC 2011: Polyhedral Geometry and Algebraic Combinatorics" ,"type":"workshop",
+						"where":"University of Wyoming",	
+						"what":" the summer school ", "when":"20 June to 1 July 2011"
+					}],
+					"year": "2011",
+					"month":"6",
+					"day":"25"
+				},
+				{
+					lat: 35.7780109,
+					lng: -78.6845589,
+					title: 'North Carolina State University',
+					"info": [{"title":"Research Visit" ,"type":"visit",
+						"where":"NCSU",	"whom":"Prof. Carla Savage", "from":"5",
+						"to" : "12 of August 2013"
+					},
+					{"title":"Seminar Talk" ,"type":"talk","where":"Seminar",	
+						"topic":"Interpolation and Polyhedral Geometry"
+					}],
+					"year": "2013",
+					"month":"8",
+					"day":"5"
+				},
+				{
+					lat: 40.5667077,
+					lng: -105.0840101,
+					title: 'Colorado State University',
+					"info": [{"title":"SIAM AG 13" ,"type":"workshop",
+						"where":"Colorado State University",	
+						"what":"SIAM Algebraic Geometry 2013", 
+						"from":"1",
+						"to": "4 of August 2013",
+						"talk" : ""
+					}],
+					"year": "2013",
+					"month":"8",
+					"day":"3"
+				},
+				{
+					lat: 52.2716927,
+					lng: 8.0427228,
+					title: 'Uni Osnabrueck',
+					"info": [{
+						"title":"COCOA 2013 International School on Computer Algebra ",
+						"type":"workshop",
+						"where":"Universitaet Osnabrueck ",	
+						"what":"COCOA school ",
+						"from":"10",
+						"to":"14 of June 2013",
+						"talk" : ""
+					}],
+					"year": "2013",
+					"month":"6",
+					"day":"12"	
+				},
+				{
+					lat: 52.2768304,
+					lng: 10.5367841,
+					title: 'TU Braunschweig',
+					"info": [{"title":"EuroCG 2013" ,"type":"workshop",
+						"where":"TU Braunschweig",	
+						"what":"EuroCG 2013 and working with Vissarion Fissikopoulos ",
+						"from":"17",
+						"to":"20 of March 2013",
+						"talk" : ""
+					}],
+					"year": "2013",
+					"month":"3",
+					"day":"18"					
+				},
+				{
+					lat: 36.7164516,
+					lng: -4.4748896,
+					title: 'Malaga',
+					"info": [{"title":"Applications of Computer Algebra 2013",
+						"type":"workshop",
+						"where":"Malaga",	
+						"what":"ACA 2013 ",
+						"from":"2",
+						"to":"6 of July 2013",
+						"talk" : ""
+					}],
+					"year": "2013",
+					"month":"7",
+					"day":"4"	
+				},
+				{
+					lat: 37.2970155,
+					lng: -121.8174129,
+					title: 'San Jose',
+					"info": [{
+						"title":" International Symposium on Symbolic and Algebraic Computation (ISSAC)	",
+						"type":"workshop",
+						"where":"San Jose",	
+						"what":"ISSAC 2011 ",
+						"from":"8",
+						"to":"11 of July 2013",
+						"talk" : ""
+					}],
+					"year": "2011",
+					"month":"6",
+					"day":"10"
+				},
+				{
+					lat: 45.192275,
+					lng: 5.757297,
+					title: 'Grenoble University',
+					"info": [{
+						"title":" International Symposium on Symbolic and Algebraic Computation (ISSAC)	",
+						"type":"workshop",
+						"where":"Grenoble",	
+						"what":"ISSAC 2012 ",
+						"from":"22",
+						"to":"25 of July 2013",
+						"talk" : ""
+					}],
+					"year": "2013",
+					"month":"7",
+					"day":"23"
+				},
+				{
+					lat: 43.3766876,
+					lng: -3.2164558,
+					title: 'Castro Urdiales',
+					"info": [{
+						"title":"Shapes Geometry and Algebra",
+						"type":"workshop",
+						"where":"CIEM Castro Urdiales",	
+						"what":"SAGA kick off meeting	 ",
+						"from":"17",
+						"to":"21 of November 2008",
+						"talk" : ""
+					}],
+					"year": "2008",
+					"month":"11",
+					"day":"18"	
+				},
+				{
+					lat: 48.2649816,
+					lng: 11.6678191,
+					title: 'TU Munich',
+					"info": [{
+						"title":" International Symposium on Symbolic and Algebraic Computation (ISSAC)	",
+						"type":"workshop",
+						"where":"Munich",	
+						"what":"ISSAC 2010 ",
+						"from":"25",
+						"to":"28 of July 2010",
+						"talk" : ""
+					}],
+					"year": "2010",
+					"month":"7",
+					"day":"26"
+				},
+				{
+					lat: 48.697411,
+					lng: 6.171898,
+					title: 'Nancy',
+					"info": [{"title":"EuroCG 2008" ,"type":"workshop",
+						"where":" Laboratoire Lorrain de Recherche en Informatique et ses Applications",	
+						"what":"EuroCG 2008",
+						"from":"18",
+						"to":"20 of March 2008",
+						"talk" : ""
+					}],
+					"year": "2008",
+					"month":"3",
+					"day":"19"
+				},
+				{
+					lat: 37.873913,
+					lng: -122.250563,
+					title: 'UC Berkeley',
+					"info": [{"title":"BADMath" ,"type":"workshop",
+						"what":"BADMath",
+						"where":"UC Berkeley",	"why":"Berndt Sturmfels 50th birthday"
+					}],
+					"year": "2012",
+					"month":"3",
+					"day":"17"			
+				},
+				{
+					lat: 41.206191,
+					lng: 29.0732755,
+					title: 'Koc University',
+					"info": [{"title":"Seminar Talk" ,"type":"talk","where":"Seminar",	
+						"topic":"Symbolic computation for orthogonal designs"
+					}],
+					"year": "2013",
+					"month":"1",
+					"day":"8"
+				},
+				{
+					lat: 40.890693,
+					lng: 29.378267,
+					title: 'Sabanci University',
+					"info": [{"title":"Seminar Talk" ,"type":"talk","where":"Seminar",	
+						"topic":"A geometric view on partition analysis"
+					}],
+					"year": "2013",
+					"month":"1",
+					"day":"2"
+				},
+				{
+					lat: 37.9384022,
+					lng: 27.4365766,
+					title: 'Nesin Mathematik Koyu',
+					infoWindow: {content: '5 courses on algebra and number theory during 2010,2011,2012,2015'},
+					"year": "2010",
+					"month":"8",
+					"day":"1"
+				},
+{
+					lat: 37.968196,
+					lng: 23.778687,
+					title: 'University of Athens',
+					"info": [{"title":"Seminar Talk" ,"type":"talk","where":"Seminar",	
+						"topic":"Polyhedral geometry for linear Diophantine systems"
+					}],					
+					"year": "2012",
+					"month":"10",
+					"day":"29"
+				},
+				{
+					lat: 48.3365002,
+					lng: 14.319016,
+					title: 'Johannes Kepler University',
+                "info": [{"title":"Seminar Talk" ,"type":"talk","where":"Seminar",	
+						"topic":"Various topics while a PhD student"
+					}],
+					"year": "2009",
+					"month":"1",
+					"day":"1"
+				},
+				{
+					lat: 38.538732,
+					lng: -121.732551,
+					title: 'UC Davis',
+                    "info": [{"title":"Algebra and Discrete Mathematics Seminar Talk" 
+                    ,"type":"talk","where":"Seminar",	
+						"topic":"Partition Analysis and its Polyhedral Geometry"
+					}],
+					"year": "2012",
+					"month":"4",
+					"day":"17"
+				},
+				{
+					lat: 35.0061,
+					lng: 135.76095,
+					title: 'Kyoto',
+                    "info": [{"title":"Conference Talk" ,"type":"talk","where":"SNC 2009",	
+						"topic":"Experimental evaluation and cross-benchmarking of univariate real solvers"
+					}],
+					"year": "2009",
+					"month":"1",
+					"day":"1"
+				},
+				{
+					lat: 44.97399,
+					lng: -93.227728,
+					title: 'Minnesota - IMA',
+		    "info": [{"title":"IMA Workshop" ,"type":"workshop",
+						"what":"IMA Annual Program Year Workshop Geometric and Enumerative Combinatorics",
+						"where":"IMA",	"why":""	
+					}],
+					"year": "2014",
+					"month":"11",
+					"day":"10"
+				},
+				{
+					lat: 37.034971,
+					lng: 22.1133041,
+					title: 'Applications of Computer Algebra - ACA 2015',
+		    "info": [{"title":"ACA 2015" ,"type":"conference",
+						"type":"workshop_organize",
+						"where":"Kalamata (Greece)",
+						"what":" the Applications of Computer Algebra conference and the special sessions 'Polytopes Algebra Computation' and 'GBReLA 2' ",
+						"from":"20",	"to":"23 of July 2015"
+					}],
+					"year": "2015",
+					"month":"7",
+					"day":"20"
+				}	,
+				{
+					lat: 52.5075419,
+					lng: 13.4251364,
+					title: 'BMS Summer School',
+		   			"info": [{"title":"BMS Summer School: Convex geometry - discrete and computational" ,"type":"workshop",
+						"where":"TU Berlin and FU Berlin",	
+						"what":" the BMS summer school ", "from":"29 June", "to": "10 July 2015"
+					}],
+					"year": "2015",
+					"month":"6",
+					"day":"29"
+				},{
+					lat: 48.3661888,
+					lng: 14.513888,
+					title: 'AEC Summer School',
+					"info": [{"title":"Summer School on Algorithmic and Enumerative Combinatorics" ,"type":"workshop",
+						"where":"Research Institute for Symbolic Computation (RISC)",
+						"what":" Summer School on Algorithmic and Enumerative Combinatorics",
+						"from":"27",	"to":"31 of July 2015"
+					}],
+					"year": "2015",
+					"month":"27",
+					"day":"27"					
+				},{
+					lat: 48.3661888,
+					lng: 14.513888,
+					title: 'Research Visit',
+					"info": [{"title":"Visiting RISC" ,"type":"visit",
+						"where":"Research Institute for Symbolic Computation (RISC)",
+						"what":" Partition Analysis and Polyhedral Geometry",
+						"whom": "Prof. Peter Paule",
+						"from":"1",	"to":"8 of August 2015"}],
+					"year": "2015",
+					"month":"27",
+					"day":"27",
+				}		
+			];
+		
+
+		
+      
+
+
